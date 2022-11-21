d9a7cebf104317431a21548df6b76b32a6e258f8
akhil <akhil.paul@ymail.com>
2022-11-21 10:23:42 +0530

    Adds additon feature

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        System.out.println("***MENU***\n" +
+                "Enter Option:\n" +
+                "1.Add \n" +
+                "2.Sub \n" +
+                "3.Multiply" +
+                "4.Divide ");
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        double numberOne,numberTwo;
+        switch (option){
+            case 1:
+                System.out.println("Enter two numbers to add:");
+                numberOne = sc.nextInt();
+                numberTwo = sc.nextInt();
+                System.out.println("After additon, result ="+(numberOne+numberTwo));
 
+        }
     }
 }
